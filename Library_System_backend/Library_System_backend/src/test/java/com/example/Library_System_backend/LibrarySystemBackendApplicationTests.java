f1a7e0dedbf546fb7b05a2a90262253c7736ed01
habibahefny <habiba.hefny@gmail.com>
2023-12-14 14:47:14 +0200

    Skeleton created

---
+++ Library_System_backend/Library_System_backend/src/test/java/com/example/Library_System_backend/LibrarySystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Library_System_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
