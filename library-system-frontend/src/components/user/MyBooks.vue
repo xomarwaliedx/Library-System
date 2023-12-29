3bdc73eb172dd1e611e3ae68e762714c5fb3fb6f
Omar_Walied <omar.ashoor35@gmail.com>
2023-12-29 22:10:33 +0200

    Fixing Styling

--- library-system-frontend/src/components/user/MyBooks.vue
+++ library-system-frontend/src/components/user/MyBooks.vue
@@ -26,9 +26,9 @@ export default {
   data: () => ({
     itemsPerPage: 5,
     headers: [
-      { title: 'Book Name', align: 'start', sortable: false, key: 'bookName' },
-      { title: 'Borrow Date', key: 'borrowDate', align: 'end' },
-      { title: 'Return Date', key: 'returnDate', align: 'end' },
+      { title: 'Book Name', align: 'center', sortable: false, key: 'bookName' },
+      { title: 'Borrow Date', key: 'borrowDate', align: 'center' },
+      { title: 'Return Date', key: 'returnDate', align: 'center' },
     ],
     items: [],
     loading: true,
